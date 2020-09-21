3fa8e6bc624edef7a8e9d0172fbae75b15005801
Diego A Pacheco Pinedo <dpacheco0921@gmail.com>
2020-09-21 14:43:23 -0400

    Update CaImProPi_demo.m

--- CaImProPi_demo.m
+++ CaImProPi_demo.m
@@ -1,11 +1,12 @@
 %% demo
+%% 1) add paths
 % it assumes you have already add the repository folders to your path
 addpath(genpath(pwd))
 
 % add paths of all dependencies
 % CaImAn, NoRMCorre, CMTK_matlab_wrapper
 
-%% Move to folder and Download demo data
+%% 2) Move to folder and Download demo data
 tDir = strrep(which('CaImProPi_demo'), 'CaImProPi_demo.m', '');
 cd(tDir)
 
@@ -21,18 +22,34 @@ outfilename = websave(filename, url);
 unzip(outfilename);
 clear url outfilename
     
+%% 3) process behavior videos with fictract
+%% Generate fictrac input files
+setupID = 'Alevel_2p1';
+% readout a tiff for mask or extract from video mp4
+im_mode = 1;
+
+batch_gen_fictrac_input_files(setupID, im_mode)
+
+%% Run fictrac on each file per current directory
+FileName = '20200222_3_1';
+fictracpars = [];
+fictracpars.redo = 1;
+%fictracpars.do_search_flag = 1; % to do global search
+%fictracpars.load_template_flag = 1; % to load pre-generated template
+%fictracpars.template_fn = 'template_im.jpg'; % pre-generated template image
+
+batch_fictrac_perfile(FileName, fictracpars)
+
+%% 4) process imaging videos 
 %% Test batch_tiff2mat
 FolderName = {'20161129'}; FileName = [];
 m2vpar = [];
-m2vpar.SpMode = '3DxT_song';
+m2vpar.SpMode = '3DxT_song_prv';
 m2vpar.ch2save = [1 2];
 m2vpar.Zres = 2; % Z resolution in um
-m2vpar.pixelsym = 0;
-batch_tiff2mat(FolderName, FileName, m2vpar)
+m2vpar.ch2plot = [];
 
-% FolderName = {'20190712'}; FileName = [];
-% m2vpar.SpMode = '3DxT';
-% batch_tiff2mat(FolderName, FileName, m2vpar)
+batch_tiff2mat(FolderName, FileName, m2vpar)
 
 %% Test batch_collectmetada
 FolderName = {'20161129'}; FileName = [];
@@ -40,12 +57,8 @@ cmpar = [];
 cmpar.pgate = 1;
 cmpar.pgates = 1;
 cmpar.mode = 1;
-cmpar.minframet = 900;
-batch_collectmetada(FolderName, FileName, cmpar)
 
-% FolderName = {'20190712'}; FileName = [];
-% cmpar.minframet = 900;
-% batch_collectmetada(FolderName, FileName, cmpar)
+batch_collectmetada(FolderName, FileName, cmpar)
 
 %% Test batch_NoRMCorre
 FolderName = []; FileName = [];
@@ -53,16 +66,11 @@ mcpar = [];
 mcpar.debug = 0;
 mcpar.rigidg = 1;
 mcpar.nrigidg = 0;
-mcpar.parallel = 1;
-mcpar.stack2del = 1:3;
+mcpar.stack2del = [1:3 97:100];
 mcpar.sgate = 1; %(1 = smooth and zero, 2 = smooth, 3 = zeroing, 0 = raw)
+mcpar.withinplane_flag = 1;
+
 batch_NoRMCorre(FolderName, FileName, mcpar)
-
-% FolderName = {'20161129'}; FileName = [];
-% batch_NoRMCorre(FolderName, FileName, mcpar)
-
-% plot results from motion correction
-batch_plotShiftPerStack([], [], mcpar)
 
 %% Test batch_SpaTemp_ResFilt
 FolderName = []; FileName = [];
@@ -72,11 +80,46 @@ stpar.size = [];
 stpar.newtimeres = 0.5;
 stpar.time = [];
 stpar.debug = 1;
+stpar.direction = 'invert';
+stpar.fshift = [6 6];
+stpar.idp_run_flag = 1;
 
 batch_SpaTemp_ResFilt(FolderName, FileName, stpar)
 
 %% Test batch_stitch_format_stacks_a
+% stitch serially imaged stacks, part 1, it stitchs mean image
+FolderName = []; FileName = [];
+cspfpars = [];
+cspfpars.refcha = 2;
+cspfpars.fshift = [6 6];
+
+batch_stitch_format_stacks_a(FolderName, FileName, cspfpars)
+
+%% generate brain mask based on F threshold + manual editing
+FolderName = []; FileName = [];
+bmpar = [];
+
+batch_brainmaskgen(FolderName, FileName, bmpar)
+
+iparams.dir_depth = 1;
+batch_plot_brainside_MIP(...
+    [], [], iparams)
 
 %% Test batch_stitch_format_stacks_b
+% stitch serially imaged stacks, part 2
+%   it stitchs whole 3DxT volume (it formats Data to be ready for ROI segmentation)
+FolderName = []; FileName = [];
+cstpar = [];
+cstpar.oDir = [];
+
+batch_stitch_format_stacks_b(FolderName, FileName, cstpar)
 
 %% Test batch_formatstacks
+% it formats Data to be ready for ROI segmentation
+FolderName = []; FileName = [];
+cstpar = [];
+cstpar.oDir = [];
+cstpar.idp_run_flag = 1;
+cstpar.fshift = [6 6];
+
+batch_formatstacks(FolderName, FileName, cstpar)
