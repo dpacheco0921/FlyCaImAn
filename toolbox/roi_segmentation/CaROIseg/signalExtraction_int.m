cbad31108ee236dc697f42d6dd2f84dfe4c9b657
Diego A Pacheco Pinedo <dpacheco0921@gmail.com>
2020-10-21 17:41:09 -0400

    update comments

--- toolbox/roi_segmentation/CaROIseg/signalExtraction_int.m
+++ toolbox/roi_segmentation/CaROIseg/signalExtraction_int.m
@@ -1,54 +1,68 @@
 function [inferred, filtered, raw, filtered_, raw_] = ...
     signalExtraction_int(Y, Y_, A, C, b, f, d1, d2, d3, iparams)
-% [inferred, filtered, raw, filtered_, raw_] = ...
-%    signalExtraction_int(Y, Y_, A, C, b, f, d1, d2, d3, iparams)
+% signalExtraction_int: extract the signal after CNMF is ran 
 %
-% inputs: Y raw data (d X T matrix, d # number of pixels, T # of timesteps)
+% Usage:
+% 	[inferred, filtered, raw] = ...
+%   	signalExtraction(Y, Y_, A, C, b, f, d1, d2, extractControl)
+%
+% inputs: 
+%         Y raw data (d X T matrix, d # number of pixels, T # of timesteps)
 %         Y_ raw data from extra channel (d X T matrix, d # number of pixels, T # of timesteps)
 %         A matrix of spatial components (d x K matrix, K # of components)
 %         C matrix of temporal components (K x T matrix)
-%         S matrix of deconvolved activity ((K-1) x T matrix) (optional)
 %         b matrix of background spatial components (d x options.nb, options.nb # of components)
 %         f matrix of background temporal components (options.nb x T matrix, options.nb # of components) 
-%         iparams.b_prc: percentile of baseline of signals from CNMF to be substracted(default:0.25);
-%           set to 0 to ignore this residue baseline 
-%         iparams.thr: energy fraction included in the raw signal contour (default:0.99)
-%
-% outputs: inferred: spatial weighting on the ROI pixels, unmixing, background substraction, and denoising
-%          filtered: spatial weighting on the ROI pixels, unmixing, background substraction
-%          raw: uniform spatial weighting on the ROI pixels (with threshold to remove the very low energy pixels),
-%           background substraction
+%         d1 dimension 1 
+%         d2 dimension 2 
+%         d3 dimension 3 
+%         extractControl.baselineRatio: residue baseline of signals from CNMF (default:0.25)
+%               set to 0 to ignore this residue baseline 
+%         extractControl.thr: energy fraction included in the raw signal contour (default:0.99)
 % 
+% outputs:
+%   inferred: spatial weighting on the ROI pixels, unmixing, background substraction, and denoising
+%   filtered: spatial weighting on the ROI pixels, unmixing, background substraction
+%   raw: uniform spatial weighting on the ROI pixels (with threshold to remove the very low energy pixels),
+%       background substraction
+%   filtered_: spatial weighting on the ROI pixels, unmixing, background
+%       substraction for additional channel (Y_)
+%   raw_: uniform spatial weighting on the ROI pixels (with threshold to remove the very low energy pixels),
+%       background substraction for additional channel (Y_)
+%   
+%   Each variable has these fields:
+%       df: df signal
+%       F: baseline
+%       dfof: dfof
+%       CR: information of the cell contour used in raw signal calculation
+%
 % Notes:
-% df: df signal
-% F: baseline
-% dfof: dfof
-% Ftrend: trend dusbtracted from C and added to F
-% CR: information of the cell contour used in raw signal calculation
-% Author: Eftychios A. Pnevmatikakis, and Weijian Yang
-% edited from signalExtraction_int
-% Notes:
+%   edited from signalExtraction (Author: Eftychios A. Pnevmatikakis, and Weijian Yang)
 
 % Default params
+
 % detrend signal
-p.dtype = 1; % 0 = lowpass filter, 1 = percentile filter
-p.sr = 2;
-p.lfreq = 0.002;
-p.d_prct = 20;
-p.d_window = 100;
-p.d_shift = 10;
+ipars.dtype = 1; % 0 = lowpass filter, 1 = percentile filter
+ipars.sr = 2;
+ipars.lfreq = 0.002;
+ipars.d_prct = 20;
+ipars.d_window = 100;
+ipars.d_shift = 10;
+
 % A thresholding
-p.medw = [3 3];
-p.nrgthr = 0.99;
-p.clos_op = strel('square', 3);
+ipars.medw = [3 3];
+ipars.nrgthr = 0.99;
+ipars.clos_op = strel('square', 3);
+
 % background estimation
-p.df_window = [];
-p.df_prctile = [];
+ipars.df_window = [];
+ipars.df_prctile = [];
+
 % gate to run on parallel
-p.se_parallel = true; 
+ipars.se_parallel = true; 
 
 if ~exist('iparams', 'var'); iparams = []; end
-p = loparam_updater(p, iparams);
+ipars = loparam_updater(ipars, iparams);
 
 % normalize spatial components to unit energy
 fprintf('1) Normalize A and C\n')
@@ -57,6 +71,7 @@ C2 = [C; f];
 K = size(C, 1);
 K2 = size(C2, 1);
 nA = sqrt(sum(A2.^2))';
+
 % normalize spatial components to unit energy
 A2 = A2/spdiags(nA, 0, K2, K2);
 C2 = spdiags(nA, 0, K2, K2)*C2;
@@ -67,7 +82,9 @@ f = C2(K+1:end, :);
 
 % get extra values
 fprintf('2) get AY\n')
-tic; AY = mm_fun(A, Y, [], p.se_parallel, []); toc
+tic
+AY = mm_fun(A, Y, [], ipars.se_parallel, []);
+toc
 
 % background + residue
 Yf = AY - (A'*A)*C;
@@ -77,29 +94,29 @@ Y_r = Yf - (A'*full(b))*f + C;
 
 % detrend C or C + residue: low pass, and detrend or use runing percentile
 fprintf('3) detrend Y_r (C + Yr) and C\n')
-if p.dtype == 0
+if ipars.dtype == 0
     
-    [filt_b, filt_a] = butter(2, p.lfreq/(p.sr/2), 'low');
+    [filt_b, filt_a] = butter(2, ipars.lfreq/(ipars.sr/2), 'low');
     C_Yr_detred = filtfilt(filt_b, filt_a, Y_r')';
     C_detrend = filtfilt(filt_b, filt_a, C')';
     
 else
     
-    C_Yr_detred = prctfilt(Y_r, p.d_prct, p.d_window, p.d_shift, 0);
-    C_detrend = prctfilt(C, p.d_prct, p.d_window, p.d_shift, 0);
+    C_Yr_detred = prctfilt(Y_r, ipars.d_prct, ipars.d_window, ipars.d_shift, 0);
+    C_detrend = prctfilt(C, ipars.d_prct, ipars.d_window, ipars.d_shift, 0);
     
 end
 
 fprintf('4) generate inferred and filtered signals\n')
 
 % inferred signal
-inferred.F = getFo(Yf, p.df_prctile, p.df_window) + C_detrend;
+inferred.F = getFo(Yf, ipars.df_prctile, ipars.df_window) + C_detrend;
 inferred.df = C - C_detrend;
 inferred.dfof = spdiags(inferred.F, 0, K, K)\inferred.df;
 inferred.Ftrend = C_detrend;
 
 % filtered signal
-filtered.F = getFo(Yf - Y_r + C, p.df_prctile, p.df_window) + C_Yr_detred;
+filtered.F = getFo(Yf - Y_r + C, ipars.df_prctile, ipars.df_window) + C_Yr_detred;
 filtered.df = Y_r - C_Yr_detred;
 filtered.dfof = spdiags(filtered.F, 0, K, K)\filtered.df;
 filtered.Ftrend = C_Yr_detred;
@@ -113,14 +130,16 @@ if ~isempty(Y_)
     % signal, and the signal or df is the detrended signal.
     
     fprintf('5) do steps 2 and 3 for extra channel and calculate filtered signal\n')
-    tic; AY_ = mm_fun(A, Y_, [], p.se_parallel, []); toc
+    tic
+    AY_ = mm_fun(A, Y_, [], ipars.se_parallel, []);
+    toc
     
     % detrend C or C + residue: low pass, and detrend or use runing percentile
-    if p.dtype == 0
-        [filt_b, filt_a] = butter(2, p.lfreq/(p.sr/2), 'low');
+    if ipars.dtype == 0
+        [filt_b, filt_a] = butter(2, ipars.lfreq/(ipars.sr/2), 'low');
         Yr_detred_ = filtfilt(filt_b, filt_a, AY_')';
     else
-        Yr_detred_ = prctfilt(AY_, p.d_prct, p.d_window, p.d_shift, 0);
+        Yr_detred_ = prctfilt(AY_, ipars.d_prct, ipars.d_window, ipars.d_shift, 0);
     end
     
     filtered_.F = Yr_detred_;
@@ -139,8 +158,12 @@ fprintf('6) threshold A and make it uniform (uA)\n')
 
 % spatial contour for raw signal
 rawA = A; 
-tic; [rawA, CR] = thresholdA(rawA, d1, d2, d3, ...
-    p.medw, p.nrgthr, p.clos_op, p.se_parallel); toc
+
+tic
+[rawA, CR] = thresholdA(rawA, d1, d2, d3, ...
+    ipars.medw, ipars.nrgthr, ipars.clos_op, ipars.se_parallel);
+toc
+
 nA = sqrt(sum(rawA.^2))';
 
 % normalize spatial components to unit energy
@@ -149,18 +172,20 @@ raw_bprc = (rawA'*full(b))*f;
 
 fprintf('7) get uAY\n')
 % no weighting on the ROI pixels, background substraction
-tic; raw.df = mm_fun(rawA, Y, [], p.se_parallel, []) - raw_bprc; toc
+tic
+raw.df = mm_fun(rawA, Y, [], ipars.se_parallel, []) - raw_bprc;
+toc
 
 fprintf('8) detrend uAY\n')
 % detrend df: low pass, and detrend then threshold
-if p.dtype == 0
+if ipars.dtype == 0
     raw_detrend = filtfilt(filt_b, filt_a, raw.df')';
 else
-    raw_detrend = prctfilt(raw.df, p.d_prct, p.d_window, p.d_shift, 0);
+    raw_detrend = prctfilt(raw.df, ipars.d_prct, ipars.d_window, ipars.d_shift, 0);
 end
 
 fprintf('9) generate raw signals\n')
-raw.F = getFo(raw_bprc, p.df_prctile, p.df_window) + raw_detrend;
+raw.F = getFo(raw_bprc, ipars.df_prctile, ipars.df_window) + raw_detrend;
 raw.df = raw.df - raw_detrend;
 raw.dfof = spdiags(raw.F, 0, K, K)\raw.df;
 raw.Ftrend = raw_detrend;
@@ -171,12 +196,14 @@ if ~isempty(Y_)
     
     % processed extra channel
     fprintf('10) do steps 7 and 8 for extra channel and calculate raw signal\n')
-    tic; raw_.df = mm_fun(rawA, Y_, [], p.se_parallel, []); toc
+    tic
+    raw_.df = mm_fun(rawA, Y_, [], ipars.se_parallel, []);
+    toc
     
-    if p.dtype == 0
+    if ipars.dtype == 0
         raw_detrend_ = filtfilt(filt_b, filt_a, raw_.df')';
     else
-        raw_detrend_ = prctfilt(raw_.df, p.d_prct, p.d_window, p.d_shift, 0);
+        raw_detrend_ = prctfilt(raw_.df, ipars.d_prct, ipars.d_window, ipars.d_shift, 0);
     end
     
     raw_.F = raw_detrend_;
@@ -193,6 +220,7 @@ end
 
 function [Ath, CR] = thresholdA(A, d1, d2, d3, ...
     medw, nrgthr, clos_op, se_parallel)
+% thresholdA: do spatial filtering of ROI spatial components
 
 [d, nr] = size(A);
 Ath = spalloc(d, nr, nnz(A));
@@ -325,13 +353,15 @@ end
 end
 
 function Fo = getFo(signal, df_prctile, df_window)
-% Basically computes Fo or background signal
+% getFo: computes Fo or background signal
 
 % similar to extract_DF_F
 if ~isempty(df_prctile)
     
-    if isempty(df_window) || (df_window > size(signal,2))
+    if isempty(df_window) || (df_window > size(signal, 2))
+        
         Fo = prctile(signal, df_prctile, 2);
+        
     else
         
         Fo = zeros(size(signal));
