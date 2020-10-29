909be578c68522355167e8b444e1728f740b7c8f
Diego A Pacheco Pinedo <dpacheco0921@gmail.com>
2020-10-28 20:48:11 -0400

    add functions

---
+++ toolbox/utilitites/var_vect_matrix_related/mat2uint16.m
@@ -0,0 +1,33 @@
+function Im = mat2uint16(Im, scale_flag)
+% mat2uint16: convert matrix or vector to uint16, with the option of rescaling
+%
+% Im = mat2uint16(Im, scale_flag)
+%
+% Args:
+%   Im: matrix/vector
+%   scale_flag: flag to scale
+%       (0: no scaling)
+%       (1: assumed to be 0-1 range, then multiplying by 2^16)
+%       (2: scale to min-max range, then multiplying by 2^16)
+
+if ~exist('scale_flag', 'var') || isempty(scale_flag)
+    scale_flag = 0;
+end
+
+if round(range(Im(:))) <= 1 && scale_flag == 1
+    
+    % Im range is assumed to be from 0-1
+    Im = Im*2^16;
+    
+elseif scale_flag == 2
+    
+    % normalize to min and max then rescale
+    Im = double(Im);
+    Im = (Im - min(Im(:)))/max(Im(:));
+    Im = Im*2^16;
+    
+end
+
+Im = uint16(Im);
+
+end
